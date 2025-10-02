278f4875eead00acdf5667a9efb06f2279dbfbfb
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-10-01 21:36:20 -0300

    Adiciona um novo caso de teste para a função soma

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,10 @@
 // src/app.test.js
-const soma = require('./app');
+const soma = require("./app");
 
-test('soma 1 + 2 para ser igual a 3', () => {
+test("soma 1 + 2 para ser igual a 3", () => {
   expect(soma(1, 2)).toBe(3);
+});
+
+test("Soma 5 + 5 para ser igual a 10", () => {
+  expect(soma(5, 5)).toBe(10);
 });
